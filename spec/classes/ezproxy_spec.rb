7d9f0c2709239d9f974d45a6ea507cb745553de0
Paul Bailey <paulba@stedwards.edu>
2015-04-12 22:39:00 -0500

    add custom param specs

--- spec/classes/ezproxy_spec.rb
+++ spec/classes/ezproxy_spec.rb
@@ -111,6 +111,92 @@ describe 'ezproxy' do
     end
   end
 
+  context "ezproxy class with parameter overrides" do
+    let(:facts) {{
+      :osfamily       => 'RedHat',
+      :architecture   => 'x86_64',
+      :concat_basedir => '/var/lib/puppet/concat',
+    }}
+    context 'ezproxy class with custom parameters' do
+      let(:params) {{
+        'ezproxy_group'     => 'custom_group',
+        'ezproxy_user'      => 'custom_user',
+        'install_path'      => '/custom/install/path',
+        'ezproxy_url'       => 'my.ezproxy.url',
+        'download_url'      => 'http://my.ezproxy.download/link',
+        'dependencies'      => [ 'package1', 'package2' ],
+        'proxy_by_hostname' => true,
+        'first_port'        => 9001,
+        'auto_login_ips'    => [ '1.0.0.0-1.255.255.255', '2.0.0.0-2.255.255.255' ],
+        'include_ips'       => [ '3.0.0.0-3.255.255.255', '4.0.0.0-4.255.255.255' ],
+        'exclude_ips'       => [ '5.0.0.0-5.255.255.255', '6.0.0.0-6.255.255.255' ],
+        'login_port'        => '8080',
+        'ssl'               => true,
+        'https_login'       => true,
+        'https_admin'       => true,
+        'max_lifetime'      => '360',
+        'max_sessions'      => '1000',
+        'max_vhosts'        => '5000',
+        'log_filters'       => [ '*.gif*', '*.jpg*' ],
+        'log_format'        => '%t %h %l %u "%r" %s %b "%{Referer}i" "%{user-agent}i"',
+        'log_file'          => '/var/log/ezproxy/ezproxy.log',
+        'local_users'       => [ 'user1:supersecure:admin', 'user2:coolpass:admin' ],
+        'admins'            => [ 'user3', 'user4', 'user5' ],
+        'default_stanzas'   => false,
+        'service_name'      => 'custom-service',
+        'service_status'    => 'stopped',
+        'service_enable'    => false,
+      }}
+
+      it { is_expected.to contain_group('custom_group').with_ensure('present') }
+      it { is_expected.to contain_user('custom_user').with_ensure('present') }
+      it { is_expected.to contain_file('/custom/install/path').with_ensure('directory') }
+      it { is_expected.to contain_package('package1').with_ensure('installed') }
+      it { is_expected.to contain_package('package2').with_ensure('installed') }
+      it { is_expected.to contain_exec('download ezproxy').with_creates('/custom/install/path/ezproxy') }
+      it { is_expected.to contain_file('/custom/install/path/ezproxy').with_mode('0755') }
+      it { is_expected.to contain_file('/custom/install/path/user.txt').with_ensure('file') }
+      it { is_expected.to contain_file('/custom/install/path/config.txt').with_ensure('file') }
+      it { is_expected.to contain_concat('ezproxy sites').with_path('/custom/install/path/sites.txt') }
+      it { is_expected.not_to contain_concat__fragment('Worldcat.org').with_ensure('present') }
+      it { is_expected.not_to contain_concat__fragment('WhatIsMyIP').with_ensure('present') }
+      it { is_expected.not_to contain_concat__fragment('DOI System').with_ensure('present') }
+      it { is_expected.to contain_file('/etc/init.d/custom-service').with_content(/su - custom_user "\$EZPROXY \$\*"$\n/) }
+      it { is_expected.to contain_service('custom-service').with_ensure('stopped') }
+    end
+
+    context "ezproxy class with cas authentication" do
+      let(:params) {{
+        'cas'                      => true,
+        'cas_login_url'            => 'https://my.cas.server/login',
+        'cas_service_validate_url' => 'https://my.cas.server/service-validate',
+      }}
+
+      it { is_expected.to contain_file('/usr/local/ezproxy/user.txt').with_content(/::CAS/) }
+
+    end
+
+    context "ezproxy class with ldap authentication" do
+      let(:params) {{
+        'ldap'         => true,
+        'ldap_options' => [],
+        'ldap_url'     => 'my.ldap.url',
+      }}
+
+      it { is_expected.to contain_file('/usr/local/ezproxy/user.txt').with_content(/::LDAP/) }
+
+    end
+
+    context "ezproxy class with with service management disabled" do
+      let(:params) {{
+        'manage_service' => false
+      }}
+
+      it { is_expected.not_to contain_service('ezproxy') }
+
+    end
+  end
+
   context 'unsupported operating system' do
     describe 'ezproxy class without any parameters on Solaris/Nexenta' do
       let(:facts) {{
