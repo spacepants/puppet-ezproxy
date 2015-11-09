023773d9b41ce264152c8e6366ea8370c22a5866
Paul Bailey <paulba@stedwards.edu>
2015-11-09 16:08:01 -0600

    updating acceptance tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -6,10 +6,7 @@ describe 'ezproxy class' do
     # Using puppet_apply as a helper
     it 'should work idempotently with no errors' do
       pp = <<-EOS
-      class { 'ezproxy':
-        service_status => stopped,
-        service_enable => false,
-      }
+      class { 'ezproxy': }
       EOS
 
       # Run it twice and test for idempotency
@@ -33,14 +30,16 @@ describe 'ezproxy class' do
     describe file('/usr/local/ezproxy/user.txt') do
       it { is_expected.to be_file }
     end
-    describe file('/usr/local/ezproxy/sites.txt') do
+    describe file('/usr/local/ezproxy/groups.txt') do
+      it { is_expected.to be_file }
+    end
+    describe file('/usr/local/ezproxy/group_Default.txt') do
       it { is_expected.to be_file }
     end
 
-    # ezproxy requires a license on startup which we can't test ephemerally
     describe service('ezproxy') do
-      it { is_expected.not_to be_enabled }
-      it { is_expected.not_to be_running }
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
     end
   end
 end
