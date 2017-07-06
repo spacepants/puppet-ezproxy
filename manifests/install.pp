7076d0d980f7aed4c00372c858057f9956994d83
Paul Bailey <paulba@stedwards.edu>
2017-07-06 14:37:50 -0500

    clean up install

--- manifests/install.pp
+++ manifests/install.pp
@@ -16,20 +16,21 @@ class ezproxy::install {
     gid     => $::ezproxy::ezproxy_group,
     require => Group[$::ezproxy::ezproxy_group]
   }
+
   file { $::ezproxy::install_path:
     ensure  => directory,
     owner   => $::ezproxy::ezproxy_user,
     group   => $::ezproxy::ezproxy_group,
-    mode    => '0755',
     require => User[$::ezproxy::ezproxy_user]
   }
-  $cmd = "curl -o ${::ezproxy::install_path}/ezproxy ${::ezproxy::download_url}"
+
   exec { 'download ezproxy':
-    command => $cmd,
+    command => "curl -o ${::ezproxy::install_path}/ezproxy ${::ezproxy::download_url}",
     creates => "${::ezproxy::install_path}/ezproxy",
     path    => '/sbin:/bin:/usr/sbin:/usr/bin',
     require => File[$::ezproxy::install_path]
   }
+
   file { "${::ezproxy::install_path}/ezproxy":
     ensure  => present,
     mode    => '0755',
@@ -38,12 +39,14 @@ class ezproxy::install {
     require => Exec['download ezproxy'],
     notify  => Exec['bootstrap ezproxy']
   }
-  if $::ezproxy::dependencies {
-    package { $::ezproxy::dependencies:
+
+  $::ezproxy::dependencies.each |$dependency| {
+    package { $dependency:
       ensure => installed,
       notify => Exec['bootstrap ezproxy']
     }
   }
+
   exec { 'bootstrap ezproxy':
     command     => "${::ezproxy::install_path}/ezproxy -m",
     refreshonly => true,
